7296c6eab0c9b3604778ff759477f0cadd19210c
PrajwalBaloji <prajwalbaloji2121996@gmail.com>
2025-05-31 09:52:11 +0530

    Exploring http methods

--- src/app.js
+++ src/app.js
@@ -2,14 +2,22 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send({ name: "prajwal" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Post user succesfull");
+});
+
 app.use("/test", (req, res) => {
-  res.send("test");
+  res.send("Test page");
 });
 
 app.use("/", (req, res) => {
-  res.send("One home page");
+  res.send("Home page");
 });
 
-app.listen(3000, () => {
+app.listen(7777, () => {
   console.log("Server is up and running and is listening to port 3000");
 });

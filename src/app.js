b86a1034a7604d1fc0aa09fe337398a41f96fdb2
PrajwalBaloji <prajwalbaloji2121996@gmail.com>
2025-06-03 08:19:04 +0530

    Added validators and login api and bcrypt to encrypt password

--- src/app.js
+++ src/app.js
@@ -1,19 +1,45 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const User = require("./models/user");
+const bcrypt = require("bcrypt");
 
 const app = express();
 
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  const user = new User(req.body);
+  const { firstName, lastName, emailId, password } = req.body;
+  const passwordHash = await bcrypt.hash(password, 10);
+  const user = new User({
+    firstName,
+    lastName,
+    emailId,
+    password: passwordHash,
+  });
 
   try {
     await user.save();
     res.send("user created");
   } catch (error) {
-    res.status(400).send("Error creating the user");
+    res.status(400).send(error);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+
+    const user = await User.findOne({ emailId });
+    if (!user) {
+      throw new Error("Inavlid credentials");
+    }
+    const isvalidPassword = await bcrypt.compare(password, user.password);
+    if (!isvalidPassword) {
+      throw new Error("Inavlid credentials");
+    }
+    res.send("Login successful");
+  } catch (error) {
+    res.status(400).send(error.message);
   }
 });
 
@@ -27,18 +53,28 @@ app.delete("/user", async (req, res) => {
   }
 });
 
-app.patch("/user", async (req, res) => {
-  const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
 
   try {
-    const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+    const isUpdateAllowed = Object.keys(req.body).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
+    }
+    if (data.skills.length > 10) {
+      throw new Error("Skills cannot be more then 10");
+    }
+    const updatedUser = await User.findByIdAndUpdate(userId, data, {
       returnDocument: "after",
+      runValidators: true,
     });
     res.send(updatedUser);
   } catch (error) {
-    console.log({ error });
-
-    res.status(400).send("Error updating the user");
+    res.status(400).send(error.message);
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,26 +1,67 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
+const validator = require("validator");
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
+    },
+    lastName: {
+      required: true,
+      type: String,
+    },
+    emailId: {
+      required: true,
+      type: String,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate: (value) => {
+        if (!validator.isEmail(value))
+          throw new Error("This is not a valid email");
+      },
+    },
+    password: {
+      required: true,
+      type: String,
+      // validate: (value) => {
+      //   if (!validator.isStrongPassword(value))
+      //     throw new Error("Add a strong password");
+      // },
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate: (value) => {
+        if (!["male", "female"].includes(value)) {
+          throw new Error("Not a valid gender");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default: "https://avatar.iran.liara.run/public/boy?username=Ash",
+    },
+    about: {
+      type: String,
+      default: "This is default introduction",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
